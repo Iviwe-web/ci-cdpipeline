8c8a41c23c30fd7732ee0c38d508bf78ad167a54
Sinako Brian Mzotsho <sinakobrian3@gmail.com>
2024-10-29 13:57:31 +0200

    Add project files.

---
+++ ci-cdpipeline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ci_cdpipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
